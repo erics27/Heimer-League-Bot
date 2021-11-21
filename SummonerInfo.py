a9ecc0df6d0d7d323e409111e1a07e36153d6f88
Andy H <andyhuang159@gmail.com>
2021-11-20 23:34:01 -0800

    Fixed API key declaration

--- SummonerInfo.py
+++ SummonerInfo.py
@@ -1,7 +1,6 @@
 import os
 import requests
 
-
-response = requests.get("https://na1.api.riotgames.com/lol/status/v3/shard-data?api_key=RGAPI-")
+key = str(os.environ.get("RIOT_API_KEY"))
+response = requests.get("https://na1.api.riotgames.com/lol/status/v3/shard-data?api_key=RGAPI-" + key)
 output = response.json()
-print(output)
