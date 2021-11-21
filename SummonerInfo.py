505e59498ed4dae4abe985171a3c004aa0207cf1
Andy H <andyhuang159@gmail.com>
2021-11-20 23:58:41 -0800

    Coded API request for summoner name and level

--- SummonerInfo.py
+++ SummonerInfo.py
@@ -1,6 +1,12 @@
 import os
 import requests
+import json
 
 key = str(os.environ.get("RIOT_API_KEY"))
-response = requests.get("https://na1.api.riotgames.com/lol/status/v3/shard-data?api_key=RGAPI-" + key)
-output = response.json()
+user_input = input("What is your summoner name? ")
+response = requests.get("https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + user_input
+                        + "?api_key=RGAPI-" + key)
+user_data = json.loads(response.text)
+name = user_data['name']
+summoner_level = response.json()["summonerLevel"]
+print(name + ": " + str(summoner_level))
