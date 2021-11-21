671f66e19951c29c5ea3eee7b884640376d023ec
Eric Sue <ericsue@students.cs.ubc.ca>
2021-11-20 23:20:42 -0800

    added !help function and removed test input

--- HeimerLeagueBot.py
+++ HeimerLeagueBot.py
@@ -1,9 +1,12 @@
 import discord
+import random
 
 from FactSelector import FactSelector
 
 client = discord.Client()
 select_fact = FactSelector()
+
+coin = ("heads", "tails")
 
 @client.event
 async def on_ready():
@@ -18,6 +21,8 @@ async def on_message(message):
         await message.channel.send(select_fact.get_fact())
     elif message.content.startswith('!help'):
         await message.channel.send('!fact = Gives a random league fact')
+    elif message.content.startswith('!coinflip'):
+        await message.channel.send(random.choice(coin))
 
 # uncomment this line and input password
 client.run("OTExNTAxMzU5Nzk3Mzg3Mjg1.YZiTyw.AAUNRcmbsqDgsjKWc4frEkWlbCQ")
