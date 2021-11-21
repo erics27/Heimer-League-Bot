8d87a0fa411feb1c5dddbfa81804db183e2317fe
Amighty-font <timothyfang9@gmail.com>
2021-11-21 00:34:25 -0800

    Implemented !Champion Search:

--- DataDragon.py
+++ DataDragon.py
@@ -1,32 +1,58 @@
+import string
+
 import requests, json
 
-response = requests.get('http://ddragon.leagueoflegends.com/cdn/11.23.1/data/en_US/champion.json')
-response.json()
-champRawData = json.loads(response.text)
-crd = champRawData['data']
-replaced = False;
-replaced_name = ""
-# Setting Variables
-name = input("What Champion Would you like?")
-if (" " in name):
-    replaced_name = name
-    name = name.replace(" ", "")
-    replaced = True
-desc = crd[name]['blurb']
-champId = crd[name]['id']
-ADbase = crd[name]['stats']['attackdamage']
-ADpl = crd[name]['stats']['attackdamageperlevel']
-FMS = crd[name]['stats']['movespeed']
-ASpl = crd[name]['stats']['attackspeedperlevel']
-title = crd[name]['title']
-if replaced:
-    name = replaced_name
+class DataDragon:
+    replaced = False;
+    name = ""
+    replaced_name = ""
+    desc = ""
+    champId = ""
+    ADbase = 0
+    ADpl = 0
+    FMS = 0
+    ASpl = 0
+    title = ""
 
-def getChampData():
-    output = name + " " + title + '\n' + desc
-    return output
+    def __init__(self, input):
+        # Concat input
+        index = input.find(":")
+        input = input[index + 1:len(input)]
 
-def getStats():
-    output = name + " :" + '\n' + "Base Attack Damage " + str(ADbase) + '\n' + "Attack-Damage Per-level " +\
-             str(ADpl) + '\n' + "Attack-Speed " + str(ASpl)
-    return output
+        self.name = input
+        response = requests.get('http://ddragon.leagueoflegends.com/cdn/11.23.1/data/en_US/champion.json')
+        response.json()
+        champRawData = json.loads(response.text)
+        crd = champRawData['data']
+
+        if (" " in self.name):
+            self.replaced_name = self.name
+            self.name = self.name.replace(" ", "")
+            self.replaced = True
+        self.desc = crd[self.name]['blurb']
+        self.champId = crd[self.name]['id']
+        self.ADbase = crd[self.name]['stats']['attackdamage']
+        self.ADpl = crd[self.name]['stats']['attackdamageperlevel']
+        self.FMS = crd[self.name]['stats']['movespeed']
+        self.ASpl = crd[self.name]['stats']['attackspeedperlevel']
+        self.title = crd[self.name]['title']
+        if self.replaced:
+            self.name = self.replaced_name
+
+    def compactSpaces(self, str):
+        os = ""
+        for c in str:
+            if c != " " or (os and os[-1] != " "):
+                os += c
+        return os
+
+    def getChampData(self):
+        if self.replaced:
+            self.name = self.compactSpaces(self.name)
+        output = self.name + " " + self.title + " :" + '\n' + self.desc
+        return output
+
+    def getStats(self):
+        output = self.name + " :" + '\n' + "Base Attack Damage " + str(self.ADbase) + '\n' + "Attack-Damage Per-level " +\
+                 str(self.ADpl) + '\n' + "Attack-Speed " + str(self.ASpl) + '\n' + "Attack-Speed " + str(self.FMS)
+        return output
--- HeimerLeagueBot.py
+++ HeimerLeagueBot.py
@@ -1,5 +1,6 @@
 import discord
-import DataDragon
+from DataDragon import DataDragon
+
 
 from FactSelector import FactSelector
 
@@ -18,7 +19,13 @@ async def on_message(message):
         await message.channel.send('pickle rick')
     elif message.content.startswith('!fact'):
         await message.channel.send(select_fact.get_fact())
-    elif message.content.startswith('')
+    elif message.content.startswith('!Champion Search:'):
+        input = message.content
+        select_dataDragon = DataDragon(input)
+        output = select_dataDragon.getChampData()
+        await message.channel.send(output)
+
+
 
 # uncomment this line and input password
-client.run("OTExNTAxMzU5Nzk3Mzg3Mjg1.YZiTyw.rFc6ynX4hEeAMnbwKhckGIHol8A")
+client.run("OTExNTAxMzU5Nzk3Mzg3Mjg1.YZiTyw.AAUNRcmbsqDgsjKWc4frEkWlbCQ")
