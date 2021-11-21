83613e5e554099add8e0b8ee1ec74ae6633f4a53
Andy H <andyhuang159@gmail.com>
2021-11-20 21:07:21 -0800

    Testing fetch from zenquotes API

--- HeimerLeagueBot.js
+++ HeimerLeagueBot.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js")
+const fetch = require("node-fetch")
 const client = new Discord.Client({
     intents:[Discord.Intents.FLAGS.GUILDS,
         Discord.Intents.FLAGS.GUILD_MEMBERS,
@@ -19,11 +20,26 @@ const client = new Discord.Client({
     // intents: [Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING, Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING, Discord.Intents.FLAGS.DIRECT_MESSAGES]
 })
 
+function getQuote() {
+	return fetch("https://zenquotes.io/api/random")
+		.then(res => {
+			return res.json()
+		})
+		.then(data => {
+			return data[0]["q"] + " -" + data[0]["a"]
+		})
+}
+
 client.on("ready", () => {
     console.log('logged in as ${client.user.tag}!')
 })
 
 client.on("message", msg => {
+	if (msg.author.bot) return
+
+	if (msg.content == "$inspire") {
+		getQuote().then(quote => msg.channel.send(quote))
+	}
     if (msg.content === "test") {
         msg.reply("pickle rick")
     }
